5478c6e446bf6f065e848e161931b6d0a7d690df
Ondřej Klubal <109076299+ondraklubal@users.noreply.github.com>
2025-05-30 11:41:21 +0200

    Update app_wordle.py

--- app_wordle.py
+++ app_wordle.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import gspread
-from google.oauth2.service_account import Credentialsimport 
+from google.oauth2.service_account import Credentials
 import pandas as pd
 from datetime import datetime
 
